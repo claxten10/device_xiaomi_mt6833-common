2cc5da655612e8889603ea253b106281eb24656c
Aaron Kling <webgeek1234@gmail.com>
2024-09-05 19:58:33 +0100

    mt6833-common: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -23,6 +23,17 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+	cat <<EOF >>"$1"
+		"device/xiaomi/mt6833-common",
+                "device/xiaomi/everpal",
+                "vendor/xiaomi/everpal",
+		"hardware/mediatek",
+		"hardware/mediatek/libmtkperf_client",
+		"hardware/xiaomi"
+EOF
+}
+
 # Initialize the helper for common
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
